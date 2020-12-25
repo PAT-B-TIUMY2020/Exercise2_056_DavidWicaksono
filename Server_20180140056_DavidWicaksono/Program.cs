cd96ca6a2ee15d11bc3e20f5ea734bf7182e38db
David Wicaksono <david.wicaksono.ft18@mail.umy.ac.id>
2020-12-26 02:03:44 +0700

    Done

---
+++ Server_20180140056_DavidWicaksono/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Service_20180140056_DavidWicaksono;
+using System.ServiceModel;
+using System.Windows.Forms;
+
+namespace Server_20180140056_DavidWicaksono
+{
+    class Program
+    {
+        /// <summary>
+        /// The main entry point for the application.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
